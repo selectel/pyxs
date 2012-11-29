5927d1950f4d4453a625ccc6d1a51ac5e85ba07c
Sergei Lebedev <superbobry@gmail.com>
2012-11-29 18:48:59 +0400

    Dropped 'runtests.py' in favour of explicit 'pytest' dependency

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ import os
 import subprocess
 import sys
 
-try:
-    from setuptools import setup, Command
-except ImportError:
-    from distutils.core import setup, Command
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -25,31 +23,30 @@ CLASSIFIERS = (
     "Development Status :: 2 - Pre-Alpha",
     "Intended Audience :: Developers",
     "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
 )
 
 
-class PyTest(Command):
+class PyTest(TestCommand):
     """Unfortunately :mod:`setuptools` support only :mod:`unittest`
     based tests, thus, we have to overider build-in ``test`` command
     to run :mod:`pytest`.
-
-    .. note::
-
-       Please pack your tests, using ``py.test --genscript=runtests.py``
-       before commiting, this will eliminate `pytest` dependency.
     """
-    user_options = []
-    initialize_options = finalize_options = lambda self: None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
-        errno = subprocess.call([sys.executable, "runtests.py", "tests.py"])
-        raise SystemExit(errno)
+    def run_tests(self):
+        import pytest
+        sys.exit(pytest.main(self.test_args + ["tests.py"]))
 
 
 setup(name="pyxs",
       version="0.3",
       packages=["pyxs"],
       cmdclass={"test": PyTest},
+      tests_require=["pytest"],
       platforms=["any"],
 
       author="Sergei Lebedev",
