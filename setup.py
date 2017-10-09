019d15981e6f2ad84efa72a9b6c779c6b1ff58fb
Sergei Lebedev <s.lebedev@criteo.com>
2017-10-09 12:13:56 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ CLASSIFIERS = (
 )
 
 setup(name="pyxs",
-      version="0.4.1",
+      version="0.4.2-dev",
       packages=["pyxs"],
       setup_requires=["pytest-runner"],
       tests_require=["pytest"],
